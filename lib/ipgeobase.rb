4cc51e510a933438ab8c082939b66dc2c2524216
Pavel Konkin <idmfani@gmail.com>
2023-03-06 13:43:02 +0300

    rename method ip_metadata_received to ip_metadata

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -5,7 +5,7 @@ require_relative "ipgeobase/version"
 module Ipgeobase
   class Error < StandardError; end
 
-  def ip_metadata_receive(ip_address)
+  def ip_metadata(ip_address)
     ip_address.to_s
   end
 end
